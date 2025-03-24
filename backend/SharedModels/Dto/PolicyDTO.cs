a5de72f2c2e8172b781378a570508350feb3ba00
Abraao Vinicius Vicentini <abraao.vicentini@dtidigital.com.br>
2025-03-23 22:59:04 -0300

    feat: modify to use better Dto's

--- backend/SharedModels/Dto/PolicyDTO.cs
+++ backend/SharedModels/Dto/PolicyDTO.cs
@@ -1,9 +1,10 @@
-﻿namespace SharedModels.Dto;
+﻿
+namespace SharedModels.Dto;
 
 public class PolicyDTO
 {
     public string Name { get; set; } = string.Empty;
-    public List<ConditionDto> Conditions { get; set; } = new();
+    public List<ConditionCreateDto> Conditions { get; set; } = new();
 }
 
 public class PolicyDetailDto
@@ -14,6 +15,7 @@ public class PolicyDetailDto
     public List<ConditionDto> Conditions { get; set; } = new();
 }
 
+
 public class PolicyOutputDto
 {
     public int Id { get; set; }
@@ -22,11 +24,21 @@ public class PolicyOutputDto
 
 public class ConditionDto
 {
-    public int Id { get; set; }
-    public string InputVariable { get; set; } = string.Empty; 
-    public string Operator { get; set; } = string.Empty; 
+    public int Id { get; set; } // Agora o ID será gerenciado pelo EF Core, mas ainda estará presente no DTO
+    public string InputVariable { get; set; } = string.Empty;
+    public string Operator { get; set; } = string.Empty;
     public double Value { get; set; }
-    public double? DecisionValue { get; set; } 
+    public double? DecisionValue { get; set; }
     public int? TrueConditionId { get; set; }
-    public int? FalseConditionId { get; set; } 
+    public int? FalseConditionId { get; set; }
+}
+
+public class ConditionCreateDto
+{
+    public string InputVariable { get; set; } = string.Empty;
+    public string Operator { get; set; } = string.Empty;
+    public double Value { get; set; }
+    public double? DecisionValue { get; set; }
+    public int? TrueConditionIndex { get; set; } // Índice ao invés de ID
+    public int? FalseConditionIndex { get; set; } // Índice ao invés de ID
 }
